ec790115f41a6dbb954bb35fbf06ae3f64e26be7
Wesley Darlington <wesley.darlington@gmail.com>
2024-02-16 09:24:09 -0500

    day 23

---
+++ day23a.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+import re
+
+example_input = """
+inc a
+jio a, +2
+tpl a
+inc a
+"""
+
+def parse(inp):
+    instructions = []
+    for line in inp.strip().splitlines():
+        instructions.append(re.split(r',? ', line))
+    return instructions
+example_instructions = parse(example_input)
+
+class CPU:
+    def __init__(self):
+        self.reg = {}
+        self.reg['a'] = 0
+        self.reg['b'] = 0
+        self.pc = 0
+
+def run1(cpu, instr):
+    op, r = instr[0], instr[1]
+    if len(instr) > 2:
+        offset = int(instr[2])
+    match op:
+        case 'hlf':
+            cpu.reg[r] //= 2
+            cpu.pc += 1
+        case 'tpl':
+            cpu.reg[r] *= 3
+            cpu.pc += 1
+        case 'inc':
+            cpu.reg[r] += 1
+            cpu.pc += 1
+        case 'jmp':
+            offset = int(r)
+            cpu.pc += offset
+        case 'jie':
+            if cpu.reg[r] % 2 == 0:
+                cpu.pc += offset
+            else:
+                cpu.pc += 1
+        case 'jio':
+            if cpu.reg[r] == 1:  # Jump if **one**
+                cpu.pc += offset
+            else:
+                cpu.pc += 1
+
+def run(instructions, cpu=None):
+    if not cpu:
+        cpu = CPU()
+    while cpu.pc < len(instructions):
+        instr = instructions[cpu.pc]
+        run1(cpu, instructions[cpu.pc])
+    return cpu
+assert run(example_instructions).reg['a'] == 2
+
+real_input = open('inputs/day23.input.txt').read()
+real_instructions = parse(real_input)
+
+print(run(real_instructions).reg['b'])  # => 170
